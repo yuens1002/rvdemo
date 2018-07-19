dc2fb0627a958073eedea6d1e653311bcca65543
sunny yuen <yuens1002@gmail.com>
2018-07-19 14:57:05 -0400

    remove nonessential files

--- src/lodashTest.js
+++
@@ -1,9 +0,0 @@
-import { groupBy } from "lodash-es"
-import people from "./people"
-
-const managerGroups = groupBy(people, "name")
-
-const name = document.createElement("div")
-name.innerHTML = `<pre>${JSON.stringify(managerGroups, null, 2)}</pre>`
-
-export default name
